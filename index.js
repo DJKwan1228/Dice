5bf4bf222d429c1699225b3bc4c26d11b566738c
DJKwan1228 <davidkwan1228@gmail.com>
2024-04-22 19:57:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImg = "dice" + randomNumber1 + ".png";
+var randomImageSrc = "images/" + randomDiceImg ;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSrc);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImg2 = "dice" + randomNumber2 + ".png";
+var randomImageSrc2 = "images/" + randomDiceImg2 ;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSrc2);
+
+//if draw
+if (randomNumber1 === randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Draw";
+
+}else if (randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 1 Won";
+}
+else{
+    document.querySelector("h1").innerHTML = "Player 2 Won";
+}
+  
+
